7c2a87ab2a3cda5f7f2d2f5077cc99cdccd52551
Om Dahale <om.g.dahale@gmail.com>
2021-06-26 13:05:39 +0530

    send email with celery

--- myproject/settings.py
+++ myproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djcelery_email',
     'crispy_forms',
     'core.apps.CoreConfig',
     'users.apps.UsersConfig',
@@ -145,7 +146,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
